5460280bc1dfb931904a512041b9d737f4f52d29
John Fotopulos <36635120+ma09890@users.noreply.github.com>
2019-06-08 21:01:25 +0300

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -32,10 +32,6 @@ double intializationOfTemperature(int *x,int *r,int L,double dist[dim][dim]){
   int *revstate=(int*)(malloc(sizeof(int)*dim));
   int count=0;
   while(ratio<0.8){
-      printf("%s\n","STEEEEEEP" );
-      printf("ratio==%f\n",ratio );
-      printf("count==%i\n",count );
-      printf("temp=%f\n",c );
       state=copy(x,dim);
       revstate=copy(r,dim);
       optchange2(x,r);
@@ -57,24 +53,13 @@ double intializationOfTemperature(int *x,int *r,int L,double dist[dim][dim]){
 }
 void optchange2(int *x,int *y){
   int d1=rand_int(0,dim-1);
-  //printf("%i\n",d1 );
   int stop=rand_int(2,dim-2);
   int d2=findNode(d1,x,0,stop);
-//  printf("%i\n",d2 );
-  //printf("distance==%d\n",countDist(d2,0,y,d1) );
   int distd1d2=countDist(d1,0,y,d2);
   int distd2d1=countDist(d2,0,y,d1);
-//  if(distd1d2<distd2d1){//peritoo
     x[x[d1]]=x[d2];
     set(d2,x,y,x[d1]);
     x[d1]=d2;
-  /*}
- else{
-    x[x[d2]]=x[d1];
-    set(d1,x,y,x[d2]);
-    x[d2]=d1;
-  }*/
-  //printf("distance==%d\n",countDist(d1,0,y,d2) );
   for(int i=0; i<dim; i++){
     y[x[i]]=i;
   }
@@ -82,20 +67,14 @@ void optchange2(int *x,int *y){
 void optchange3(int *x,int *y){
   int d1=rand_int(0,dim-1);
   int stop=rand_int(2,dim-2);
-  //printf("stop==%i\n",stop );
   int d2=findNode(d1,x,0,stop);
-  int d3=0;//rand_int(0,dim-1);
-//  int distd1d2=dim-countDist(d1,0,y,d2);
-//  int distd2d1=dim-countDist(d2,0,y,d1);
+  int d3=0;
   while(d3==d1 || d3==y[d1] || d3==x[d1] || d3==d2 ||d3==x[d2]|| d3==y[d2]){
     d3=rand_int(0,dim-1);
   }
-  //printf(" d1=%i\n d2=%i\n d3=%i\n ",d1,d2,d3 );
-
   int dist1=dim-countDist(d1,0,y,d2);
   int dist2=dim-countDist(d1,0,y,d3);
-  //printf("%i\n %i\n",dist1,dist2 );
-
+	
   if(dist1<=dist2){
     makePermutation(d1,d2,d3,x,y);
   }
@@ -109,7 +88,6 @@ void optchange3(int *x,int *y){
 void makePermutation(int d1,int d2,int d3,int *x,int*y){
   int c=rand_int(0,3);
   int t=0;
-  //printf("case=%i\n",c );
   switch (c) {
     case  0:
             x[x[d1]]=x[d2];
@@ -152,7 +130,6 @@ int findNode(int t,int* x,int d,int stop){
 }
 void set(int t,int*x ,int*y,int stop){
   if(t==stop) return;
-  //printf("x=%i y=%i t=%i ",x[t],y[t] ,t);
   x[t]=y[t];
   set(y[t],x,y,stop);
 }
